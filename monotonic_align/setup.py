77d1ba52bb894f8b4c3775de7822e159fca28116
SanjanaHukkeriIIITH <sanjana.hukkeri@research.iiit.ac.in>
2025-12-01 16:58:56 +0530

    resemble ai mas

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,21 +1,27 @@
-from setuptools import setup, Extension
+import numpy
+from setuptools import Extension, find_packages
+from distutils.core import setup
 from Cython.Build import cythonize
-import numpy as np
-import os
 
-ext_modules = [
-    Extension(
-        "monotonic_align.core",
-        ["core.pyx"],
-        include_dirs=[np.get_include()],
-    )
-]
+
+_VERSION = "1.2"
+
+
+ext_modules = cythonize(
+  ["monotonic_align/core.pyx",
+   "monotonic_align/core1alt.pyx",
+   "monotonic_align/core2.pyx",
+   "monotonic_align/core2eps.pyx",
+  ],
+  compiler_directives={"language_level": "3"},
+)
 
 setup(
-    name="monotonic_align",
-    ext_modules=cythonize(
-        ext_modules,
-        compiler_directives={'language_level': "3"}
-    ),
-    packages=["monotonic_align"],
+  name="monotonic_align",
+  ext_modules=ext_modules,
+  include_dirs=[numpy.get_include(), "monotonic_align"],
+  packages=find_packages(),
+  setup_requires=["numpy", "cython"],
+  install_requires=["numpy"],
+  version=_VERSION,
 )
